ded9b7b2397dbe2e383e507531b9e810ab20ef1a
Saleh1311 <61668515+Saleh1311@users.noreply.github.com>
2020-09-09 22:38:44 +0300

    Add files via upload

---
+++ Add student for coop/Assiment5/Assiment5/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Assiment5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gpa = new System.Windows.Forms.TextBox();
+            this.major = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.list = new System.Windows.Forms.ListView();
+            this.h1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.h2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.h3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DeleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.gpa);
+            this.groupBox1.Controls.Add(this.major);
+            this.groupBox1.Controls.Add(this.name);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 112);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add new student to COOP";
+            // 
+            // gpa
+            // 
+            this.gpa.Location = new System.Drawing.Point(52, 89);
+            this.gpa.Name = "gpa";
+            this.gpa.Size = new System.Drawing.Size(124, 20);
+            this.gpa.TabIndex = 6;
+            // 
+            // major
+            // 
+            this.major.Location = new System.Drawing.Point(52, 57);
+            this.major.Name = "major";
+            this.major.Size = new System.Drawing.Size(124, 20);
+            this.major.TabIndex = 5;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(52, 25);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(124, 20);
+            this.name.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "GPA :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Major :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name :";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(15, 132);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 32);
+            this.panel1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Image = global::Assiment5.Properties.Resources.icons8_add_30px;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.MinimumSize = new System.Drawing.Size(200, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 35);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Add";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // list
+            // 
+            this.list.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.h1,
+            this.h2,
+            this.h3});
+            this.list.HideSelection = false;
+            this.list.Location = new System.Drawing.Point(14, 170);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(216, 143);
+            this.list.TabIndex = 4;
+            this.list.UseCompatibleStateImageBehavior = false;
+            this.list.View = System.Windows.Forms.View.Details;
+            this.list.MouseClick += new System.Windows.Forms.MouseEventHandler(this.fff);
+            // 
+            // h1
+            // 
+            this.h1.Text = "Name";
+            this.h1.Width = 70;
+            // 
+            // h2
+            // 
+            this.h2.Text = "Major";
+            this.h2.Width = 70;
+            // 
+            // h3
+            // 
+            this.h3.Text = "GPA";
+            this.h3.Width = 70;
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.DeleteToolStripMenuItem});
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(108, 48);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // DeleteToolStripMenuItem
+            // 
+            this.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem";
+            this.DeleteToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.DeleteToolStripMenuItem.Text = "Delete";
+            this.DeleteToolStripMenuItem.Click += new System.EventHandler(this.DeleteToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 318);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Add";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox gpa;
+        private System.Windows.Forms.TextBox major;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView list;
+        private System.Windows.Forms.ColumnHeader h1;
+        private System.Windows.Forms.ColumnHeader h2;
+        private System.Windows.Forms.ColumnHeader h3;
+        private System.Windows.Forms.ContextMenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem DeleteToolStripMenuItem;
+    }
+}
+
---
+++ Add student for coop/Assiment5/Assiment5/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Forms;
+
+namespace Assiment5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+            list.Items.Add(name.Text).SubItems.AddRange(new string[] { major.Text, gpa.Text });
+        }
+
+       
+
+
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (list.SelectedItems.Count > 0)
+            {
+                int var2 = list.Items.IndexOf(list.SelectedItems[0]);
+                list.Items.Add(list.Items[var2].Text).SubItems.AddRange(new string[] { list.SelectedItems[0].SubItems[1].Text.ToString(), list.SelectedItems[0].SubItems[2].Text.ToString() });
+                
+            }
+
+           
+
+        }
+
+       
+
+      
+
+     
+
+        private void DeleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < list.Items.Count; i++)
+            {
+                if (list.Items[i].Selected)
+                {
+                    list.Items[i].Remove();
+                    i--;
+                }
+            }
+        }
+
+        private void fff(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < list.Items.Count; i++)
+            {
+                if (list.Items[i].Selected)
+                {
+                    menu.Show(Cursor.Position);
+                    break;
+                    
+                }
+            }
+        }
+        }
+    }
+
+
